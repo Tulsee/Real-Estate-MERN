adee3a1de2f9750556c8afd1571e85476534e268
tulsee <ramtulsi022@gmail.com>
2023-12-29 15:47:26 +0545

    get user created listing list api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/User.model.js";
+import Listing from "../models/Listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const test = (req, res) => {
@@ -46,5 +47,18 @@ export const deleteUser = async (req, res, next) => {
       .clearCookie("access_token");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!!!"));
   }
 };
